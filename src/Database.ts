fcbef8eee7a19af21c599664dc4609ba79f38d38
Krzysztof Wrona <rubikon02@gmail.com>
2022-12-17 03:10:52 +0100

    Add login

--- src/Database.ts
+++ src/Database.ts
@@ -1,14 +1,16 @@
 import mysql from "mysql"
 
+export type QueryResult<T> = { success: false, error: string | undefined } | { success: true, data: T[] };
+
 const host = "10.200.2.96"
 const user = "sheepyourhack4"
 const password = "h4VktwjGtf5cjy"
 const database = "sheepyourhack4"
 
 export default class Database {
-    connection: mysql.Connection | null = null
+    connection: mysql.Connection
 
-    connect() {
+    constructor() {
         this.connection = mysql.createConnection({
             host: host,
             user: user,
@@ -22,12 +24,33 @@ export default class Database {
         });
     }
 
-    get_questions() {
-        if (!this.connection) return;
-        const sql = "SELECT * FROM questions"
-        this.connection.query(sql, (err, result) => {
-            if (err) throw err;
-            console.log("Result: ", result);
-        });
+    async login(username: string, password: string) {
+        const sql = "SELECT id FROM users WHERE username=? AND password=?"
+        return await this.query<{ id: number }>(sql, [username, password]);
+    }
+
+    // get_questions() {
+    //     const sql = "SELECT * FROM questions"
+    //     this.connection.query(sql, (err, result) => {
+    //         if (err) throw err;
+    //         console.log("Result: ", result);
+    //     });
+    // }
+
+    query = <T>(query: string, values: any[] = []) => {
+        return new Promise<QueryResult<T>>((resolve) => {
+            return this.connection.query(query, values, (err, result) => {
+                if (err) {
+                    return resolve({
+                        success: false,
+                        error: err.sqlMessage
+                    });
+                }
+                return resolve({
+                    success: true,
+                    data: result
+                });
+            });
+        })
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,41 @@
 import Express from "express"
 import Database from "./Database";
+import bodyParser from "body-parser";
 
 const app = Express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
 const database = new Database();
 
-database.connect()
-database.get_questions()
+// database.get_questions()
+database.login("admin", "admin")
 
 app.get("/", (_, res) => {
     res.send("Hello world!");
 })
+
+app.post("/login", async (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send({ description: "Wystąpił błąd logowania" });
+    }
+
+    const username = req.body.username.toString()
+    const password = req.body.password.toString()
+    const result = await database.login(username, password)
+
+    if (result.success === false || result.data.length !== 1) {
+        return res.status(400).send({ description: "Wystąpił błąd logowania" });
+    }
+
+    res.cookie("id", result.data[0].id)
+
+    return res.send({ description: "Logowanie powiodło się" });
+})
+
 
 app.listen(3000, () => {
     console.log("Listening on 3000");
